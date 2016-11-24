1e7adf45faf1926c4bc82cff37ce4226de30db77
Mark Jones <mpjonesemail@gmail.com>
2016-11-24 13:56:55 +0000

    backup

--- js/notes.js
+++ js/notes.js
@@ -15,6 +15,10 @@ var note = document.getElementById('note');
 var saveNoteButton = document.getElementById('saveNote');
 var mySavedNotesPanel = document.getElementById('mySavedNotes');
 var myNotesButton = document.getElementById('myNotes');
+var relatedNotesButton = document.getElementById('relatedNotes');
+var confirm = document.getElementsByName('note')[0];
+var myNotesList = mySavedNotesPanel.getElementsByTagName("ul")[0];
+var noteListItems;
 /*////////////////////////////////////////
 Sets the last note, for use in "pick up where you left off"  lastVidPlayed.onclick = function() in main.js
  ////////////////////////////////////////*/
@@ -31,7 +35,7 @@ if (typeof Cookies('storedNotes') !== 'undefined') {
 //                                                                                                  //
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 /*////////////////////////////////////////
-Save current note into array as an object
+On save, Assemble the note into an object and send on to be saved
  ////////////////////////////////////////*/
 saveNoteButton.onclick = function(e) {
     e.preventDefault();
@@ -46,25 +50,29 @@ saveNoteButton.onclick = function(e) {
         alert("Nothing to save");
     }
 };
-
+/*////////////////////////////////////////
+On save, Assemble the note into an object and send on to be saved
+ ////////////////////////////////////////*/
 function findRelatedNotes(thisNote) {
     console.log('findRelatedNotes has been called');
     if (myNotes.length > 0) {
         //console.log('case 1');
-        var flag = false;
+        var flag = false; //initialise flag / flag state reset
+        // loop through exsisting notes. If note title exsists, then append note.
         for (var i = 0; i < myNotes.length; i++) {
             //console.log('loop started');
             if (myNotes[i].title == thisNote.title) {
-                flag = true;
+                flag = true; //prevent note from being pushed as a new entry
                 console.log('found match');
                 console.log('appending to note');
                 var currentNoteVal = myNotes[i].note;
-                var newNoteVal = currentNoteVal + '<br/><br/>' + thisNote.note;
-                console.log(newNoteVal);
+                var newNoteVal = currentNoteVal + '<br/><br/><strong>' + dateString + '</strong> | ' + thisNote.note;
+                //console.log(newNoteVal);
                 myNotes[i].note = newNoteVal;
                 Cookies.set('storedNotes', myNotes);
                 //myNotes = Cookies.getJSON('storedNotes');
                 console.log('Saved');
+                confirm.placeholder = 'The note for this subject has been updated';
                 note.value = "";
             }
         }
@@ -77,14 +85,17 @@ function findRelatedNotes(thisNote) {
     }
 }
 
-
+/*////////////////////////////////////////
+If its a new subject, add to notes as an array item
+ ////////////////////////////////////////*/
 function pushNote(thisNote) {
     //console.log('push has been called');
     myNotes.push(thisNote);
     Cookies.set('storedNotes', myNotes);
-    myNotes = Cookies.getJSON('storedNotes');
-    note.value = "";
+    //myNotes = Cookies.getJSON('storedNotes');
     console.log('New Note Saved');
+    confirm.placeholder = 'New note saved';
+    note.value = "";
 }
 
 
@@ -94,42 +105,66 @@ Load all notes and trigger My Notes panel into view
 myNotesButton.onclick = function(e) {
     e.preventDefault();
     //create UL and add class
-    var myNotesList = mySavedNotesPanel.getElementsByTagName("ul")[0];
+
     var newUlClass = document.createAttribute("class");
     newUlClass.value = "saved-notes-list";
     myNotesList.setAttributeNode(newUlClass);
     //clear the list to allow regeneration
     myNotesList.innerHTML = "";
     //loop through notes array and create list items to append
+    myNotes.reverse(); //Newest first
     for (var i = 0; i < myNotes.length; i++) {
-        //set up li
-        var mySavedNote = document.createElement('li');
-        //get note details to populate the li
-        var noteTitle = myNotes[i].title;
-        var note = myNotes[i].note;
-        var noteDate = myNotes[i].date;
-        var noteId = myNotes[i].id;
-        myNoteslistItem = [
-            '<p class="note-title">' + noteTitle + '</p>',
-            '<p class="date">' + noteDate + '</p>',
-            '<p class="note">' + note + '</p>'
-        ].join('\n');
-        myNotesList.appendChild(mySavedNote);
-        //add class to li's
-        var newClass = document.createAttribute('class');
-        newClass.value = "saved-notes-list-item";
-        mySavedNote.setAttributeNode(newClass);
-        //add id to li's
-        var newId = document.createAttribute('id');
-        newId.value = noteId;
-        mySavedNote.setAttributeNode(newId);
-        //push to list
-        mySavedNote.innerHTML = myNoteslistItem;
-        myNotesList.appendChild(mySavedNote);
+        displayNotes(myNotes[i]);
     }
+    myNotes.reverse(); //Put array back in correct order - required to save on sorting ID's when pushing new note.
     // add class to trigger panel show
     mySavedNotesPanel.classList.add('notes-open');
 };
+
+function displayNotes(noteToBuild) {
+    //set up li
+    var mySavedNote = document.createElement('li');
+    //get note details to populate the li
+    var noteTitle = noteToBuild.title;
+    var note = noteToBuild.note;
+    var noteDate = noteToBuild.date;
+    var noteId = noteToBuild.id;
+    myNoteslistItem = [
+        '<p class="note-title">' + noteTitle + '</p>',
+        '<p class="date">' + noteDate + '</p>',
+        '<i id="' + noteId + '" class="fa fa-pencil-square fa-lg edit-note" aria-hidden="true"></i>',
+        '<p class="saved-note">' + note + '</p>'
+    ].join('\n');
+    myNotesList.appendChild(mySavedNote);
+    //add class to li's
+    var newClass = document.createAttribute('class');
+    newClass.value = "saved-notes-list-item";
+    mySavedNote.setAttributeNode(newClass);
+    //add id to li's
+    var newId = document.createAttribute('id');
+    newId.value = 'note-' + noteId;
+    mySavedNote.setAttributeNode(newId);
+    //push to list
+    mySavedNote.innerHTML = myNoteslistItem;
+    myNotesList.appendChild(mySavedNote);
+    /////////////////////////////////////////////////////////////////////////////
+    var editNote = document.getElementById(noteId);
+
+    editNote.onclick = function(e) {
+        e.preventDefault();
+        console.log(editNote);
+        var noteDetailPanel = document.getElementById('note-detail-panel');
+        var noteId = this.id;
+        console.log(noteId);
+        for (var i = 0; i < myNotes.length; i++) {
+            if (myNotes[i].id == noteId) {
+                console.log(myNotes[i]);
+                noteDetailPanel.innerHTML = myNotes[i].note;
+            }
+        }
+        noteDetailPanel.classList.add('active');
+    };
+}
 /*////////////////////////////////////////
 close saved notes panel
  ////////////////////////////////////////*/
@@ -138,7 +173,28 @@ closeSavedNotes.onclick = function(e) {
     e.preventDefault();
     mySavedNotesPanel.classList.remove('notes-open');
 };
+/*////////////////////////////////////////
+find notes relative to current notepad title
+ ////////////////////////////////////////*/
+relatedNotesButton.onclick = function(e) {
+    e.preventDefault();
+    var flag = false;
+    var findThis = document.getElementById('noteTitle').value;
+    for (var i = 0; i < myNotes.length; i++) {
+        if (myNotes[i].title == findThis) {
+            flag = true;
+            myNotesList.innerHTML = "";
+            console.log('Match Found');
+            displayNotes(myNotes[i]);
+            mySavedNotesPanel.classList.add('notes-open');
+            break;
+        }
 
+    }
+    if (flag === false) {
+        console.log('No Related Notes Found');
+    }
+};
 /*////////////////////////////////////////
 Open a note from the saved notes panel
  ////////////////////////////////////////*/
